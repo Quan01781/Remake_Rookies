5c83342b166b89f08b6b1c46a8784f80037aa8a8
Quan01781 <nguyenhoangzeroquan@gmail.com>
2023-03-27 19:28:20 +0700

    modify model

--- API-web/Models/Category.cs
+++ API-web/Models/Category.cs
@@ -8,6 +8,7 @@ namespace API_web.Models
         public int Id { get; set; }
         [Required]  
         public string Name { get; set; }
-        public ICollection<Product>? Products { get; set; }
+        public string Description { get; set; }
+        public ICollection<Product>? Products { get; set; }       
     }
 }
